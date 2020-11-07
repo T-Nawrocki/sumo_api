e5b1da4dbf7ab53f8bb400188a70926478e95d8e
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-11-07 16:10:26 +0000

    rikishi show urls use urls.py rather than hard coding, and are now namespaced to the app

--- rikishi/urls.py
+++ rikishi/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'rikishi'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:rikishi_id>/', views.show, name='show')
