e76d1d2462c324d1df89ae1422748a668d8bca0e
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-11-07 22:47:03 +0000

    heya data added

--- rikishi/admin.py
+++ rikishi/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Rikishi
+from .models import Rikishi, Heya
 
 # Register your models here.
 admin.site.register(Rikishi)
+admin.site.register(Heya)
