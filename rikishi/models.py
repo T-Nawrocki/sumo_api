228cf1b0d869a02facc127a31f10349be19a67fa
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-11-07 21:17:51 +0000

    heya model added with consequential changes to rikishi model

--- rikishi/models.py
+++ rikishi/models.py
@@ -2,29 +2,31 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.utils.translation import gettext_lazy as _
 
+
 class Rank(models.TextChoices):
-    YOKOZUNA = 'Y', _('Yokozuna')
-    OZEKI = 'O', _('Ōzeki')
-    SEKIWAKE = 'S', _('Sekiwake')
-    KOMUSUBI = 'K', _('Komusubi')
-    MAEGASHIRA_1 = 'M1', _('Maegashira 1')
-    MAEGASHIRA_2 = 'M2', _('Maegashira 2')
-    MAEGASHIRA_3 = 'M3', _('Maegashira 3')
-    MAEGASHIRA_4 = 'M4', _('Maegashira 4')
-    MAEGASHIRA_5 = 'M5', _('Maegashira 5')
-    MAEGASHIRA_6 = 'M6', _('Maegashira 6')
-    MAEGASHIRA_7 = 'M7', _('Maegashira 7')
-    MAEGASHIRA_8 = 'M8', _('Maegashira 8')
-    MAEGASHIRA_9 = 'M9', _('Maegashira 9')
-    MAEGASHIRA_10 = 'M10', _('Maegashira 10')
-    MAEGASHIRA_11 = 'M11', _('Maegashira 11')
-    MAEGASHIRA_12 = 'M12', _('Maegashira 12')
-    MAEGASHIRA_13 = 'M13', _('Maegashira 13')
-    MAEGASHIRA_14 = 'M14', _('Maegashira 14')
-    MAEGASHIRA_15 = 'M15', _('Maegashira 15')
-    MAEGASHIRA_16 = 'M16', _('Maegashira 16')
-    MAEGASHIRA_17 = 'M17', _('Maegashira 17')
-    MAEGASHIRA_18 = 'M18', _('Maegashira 18')
+  YOKOZUNA = 'Y', _('Yokozuna')
+  OZEKI = 'O', _('Ōzeki')
+  SEKIWAKE = 'S', _('Sekiwake')
+  KOMUSUBI = 'K', _('Komusubi')
+  MAEGASHIRA_1 = 'M1', _('Maegashira 1')
+  MAEGASHIRA_2 = 'M2', _('Maegashira 2')
+  MAEGASHIRA_3 = 'M3', _('Maegashira 3')
+  MAEGASHIRA_4 = 'M4', _('Maegashira 4')
+  MAEGASHIRA_5 = 'M5', _('Maegashira 5')
+  MAEGASHIRA_6 = 'M6', _('Maegashira 6')
+  MAEGASHIRA_7 = 'M7', _('Maegashira 7')
+  MAEGASHIRA_8 = 'M8', _('Maegashira 8')
+  MAEGASHIRA_9 = 'M9', _('Maegashira 9')
+  MAEGASHIRA_10 = 'M10', _('Maegashira 10')
+  MAEGASHIRA_11 = 'M11', _('Maegashira 11')
+  MAEGASHIRA_12 = 'M12', _('Maegashira 12')
+  MAEGASHIRA_13 = 'M13', _('Maegashira 13')
+  MAEGASHIRA_14 = 'M14', _('Maegashira 14')
+  MAEGASHIRA_15 = 'M15', _('Maegashira 15')
+  MAEGASHIRA_16 = 'M16', _('Maegashira 16')
+  MAEGASHIRA_17 = 'M17', _('Maegashira 17')
+  MAEGASHIRA_18 = 'M18', _('Maegashira 18')
+
 
 class Rikishi(models.Model):
   
@@ -38,20 +40,36 @@ class Rikishi(models.Model):
   total_wins = models.PositiveIntegerField()
   total_losses = models.PositiveIntegerField()
   total_absent = models.PositiveIntegerField()
+  heya = models.ForeignKey("rikishi.Heya", null=True, on_delete=models.SET_NULL)
   real_name_first = models.CharField(max_length=50)
   real_name_last = models.CharField(max_length=50)
   date_of_birth = models.DateField()
   height = models.IntegerField(validators=[MinValueValidator(150), MaxValueValidator(250)])
   weight = models.DecimalField(max_digits=4, decimal_places=1)
   origin = models.CharField(max_length=100)
-  
-  def __str__(self):
-    return self.shikona()
-  
+
   def shikona(self):
     return f'{ self.shikona_first } { self.shikona_last }'
 
   def real_name(self):
     return f'{ self.real_name_first } { self.real_name_last }'
   
+  class Meta:          
+    verbose_name = _("rikishi")
+    verbose_name_plural = _("rikishi")
 
+  def __str__(self):
+    return self.shikona()
+  
+
+class Heya(models.Model):
+
+    name = models.CharField(max_length=50)
+    master = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = _("heya")
+        verbose_name_plural = _("heyas")
+
+    def __str__(self):
+        return self.name
